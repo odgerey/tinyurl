c4cba696a81e7dbb56bc47671f04f03bc27ea5b4
odgerey <audreymedaino@gmail.com>
2020-06-08 21:51:23 +0000

    Added html coding & a views folder for ejs files

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -18,6 +20,15 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+// app.post('/', function (req, res) {
+//   res.send('POST request to the homepage')
+// })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
