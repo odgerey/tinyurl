e930553f2e5e4671d26e27fe670e19ab95c0f9ae
odgerey <audreymedaino@gmail.com>
2020-06-09 17:22:02 +0000

    Added nodemon to the json package

--- express_server.js
+++ express_server.js
@@ -4,10 +4,18 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+
+function generateRandomString() {
+return Math.random().toString(36).substring(7)
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -35,9 +43,27 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  if (longURL){
+  res.redirect(longURL);
+  } else {
+    res.status(404);
+    res.end();
+  }
+});
+
+
 // app.post('/', function (req, res) {
 //   res.send('POST request to the homepage')
 // })
+// save to the url database
+app.post("/urls", (req, res) => {
+  const tiny = generateRandomString() 
+  urlDatabase[tiny] = req.body.longURL
+  res.redirect(`/urls/${tiny}`)  // Respond with 'Ok' (we will replace this)
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
