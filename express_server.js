08aa0bd627b269dc05d4f1d04eac6e4cc36a6149
odgerey <audreymedaino@gmail.com>
2020-06-10 21:34:14 +0000

    Added a registration page, created global functions to callback newUser and findUserByEmail, debugging logout & registration cookie problems

--- express_server.js
+++ express_server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser')
-app.set("view engine", "ejs");
 
-
+//Generates the Short URL unique ID
 function generateRandomString() {
 return Math.random().toString(36).substring(7)
+};
+//Generates the User unique ID
+function generateRandomId() {
+  return Math.random().toString(36).substring(7)
 };
 
 const urlDatabase = {
@@ -14,27 +18,63 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const bodyParser = require("body-parser");
+const userDatabase = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+const findUserByEmail = function (email) {
+for (let uniqueUser of userDatabase) {
+  if (userDatabase[uniqueUser].email === email) {
+    return usersDatabase[uniqueUser];
+  }
+}
+return false; 
+};
+
+const createNewUser = function (name, email, password) {
+  const userId = generateRandomId();
+  const newUser = {
+    id: userId, 
+    email,
+    password,
+  };
+  userDatabase[userId] = newUser;
+  return userId
+};
+
+//Express extensions used by my APP 
 app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 app.use(cookieParser())
 
+//Not needed
 app.get("/", (req, res) => {
   res.send("Hello!");
-});
-
-// res.json = json.stringify(object) + res.send
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+
+
+// res.json = json.stringify(object) + res.send
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 app.get("/urls", (req, res) => {
-  console.log(req.cookies)
   let templateVars = { 
-    username: req.cookies["username"],
+    userId: req.cookies["user_id"],
     urls: urlDatabase 
   };
   res.render("urls_index", templateVars);
@@ -42,14 +82,14 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"]
+    userId: req.cookies["user_id"]
   }
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { 
-    username: req.cookies["username"],
+    userId: req.cookies["user_id"],
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
@@ -65,14 +105,42 @@ app.get("/u/:shortURL", (req, res) => {
   }
 });
 
+app.get("/register", (req, res) => {
+  let templateVars = {
+    userId: req.cookies["user_id"]
+  }
+  res.render("urls_registration", templateVars);
+});
+
+//WHY IS IT GENERATING A NEW STRING INTO THE SHORT STRING
+app.post ("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+ 
+  const user = findUserByEmail(email);
+  if (!user) {
+    const userId = createNewUser(name, email, password);
+    res.cookie('user_id', userId);
+    res.redirect("/urls");
+  } else {
+    res.status(403).send('Oups! It seems you are already registered.')
+    setTimeout(function(){
+      res.redirect("/urls")
+    }, 10000);
+  }
+});
 
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
+//const loggedInUser = userDatabase[userId];
+//WHY IS REQ.BODY.USERNAME STILL AN OBJECT
+console.log(req.body)
+  res.cookie("user_id", req.body.email);
   res.redirect("/urls")
 })
 
+
 app.post("/logout", (req, res) => {
-  res.clearCookie();
+  res.clearCookie("user_id");
   res.redirect("/urls")
 })
 
